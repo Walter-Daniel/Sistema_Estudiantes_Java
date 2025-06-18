2f4f8dd717662bf6f8ade0cdd05eabebd03b0ee1
Walter Daniel Carrizo <walterdcarrizo.19@gmail.com>
2025-06-18 17:15:52 -0300

    Initial project setup with domain and connection layers

---
+++ src/main/java/dev/purple/domain/Student.java
@@ -0,0 +1,82 @@
+package dev.purple.domain;
+
+public class Student {
+    private int idStudent;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+
+    public Student() {
+    }
+
+    public Student(int idStudent) {
+        this.idStudent = idStudent;
+    }
+
+    public Student(String firstName, String lastName, String phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public Student(int idStudent, String firstName, String lastName, String phoneNumber, String email) {
+        this.idStudent = idStudent;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public int getIdStudent() {
+        return idStudent;
+    }
+
+    public void setIdStudent(int idStudent) {
+        this.idStudent = idStudent;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "idStudent=" + idStudent +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
